a1fe9a1e831073ded878b79fdf4757096a91212c
Ems <emsardellini@gmail.com>
2026-08-09 10:15:35 +0200

    Aggiungere i file di progetto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Helper_Asta
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
